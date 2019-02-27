306c04ce22f0946dd2567a1bce7052e88180f0f4
Michael Adler <michael.adler@spirit-testing.com>
2019-02-27 17:20:16 +0100

    XH-Request zum Abholen der Nachrichten funktioniert

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function toggleText() {
-	toggleDisplayAttribute(document.getElementById('inhalt'));
+const serverAdresse = "http://spirit-academy.herokuapp.com/api/echo";
+
+function aktualisieren() {
+	// Verbindung zum Nachrichtenserver aufbauen und Nachrichten abrufen
+	var request = new XMLHttpRequest();
+	request.addEventListener("load", onLoadListener);
+	request.open("GET", serverAdresse);
+	request.send();
 }
 
-function toggleDisplayAttribute(element) {
-	if (getDisplayAttribute(element) === 'none') {
-		setDisplayAttribute(element, 'block');
-	} else {
-		setDisplayAttribute(element, 'none');
-	}
-}
+function onLoadListener() {
+	// Inhaltselement identifizieren und Referenz bekommen
+	var inhaltselement = document.getElementById('inhalt');
+	var json = JSON.parse(this.responseText);
+	var neuerInhalt = "<ul>";
+	json.forEach(function(nachricht) {
+		neuerInhalt = neuerInhalt + "<li>" + nachricht + "</li>";
+	});
+	neuerInhalt = neuerInhalt + "</ul>";
 
-function getDisplayAttribute(element) {
-	return element.style['display'];
-}
-
-function setDisplayAttribute(element, value) {
-	element.style['display'] = value;
+	inhaltselement.innerHTML = neuerInhalt;
 }
